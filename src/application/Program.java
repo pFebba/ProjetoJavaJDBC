138ab4d96ded088bb2b9633512891089b05bc3c6
pFebba <pedro.febba@outlook.com>
2024-06-25 16:09:18 -0300

    Criando classe 'seller'

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate date = LocalDate.parse("11/06/2006", dtf);
 		
-		System.out.println(obj);
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", date, 3000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
